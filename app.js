78a0ea9f658e031932bac2248f53a3939ca396d2
neeerajtk <neerajtk560@gmail.com>
2020-04-05 22:56:51 +0530

    img added

--- app.js
+++ app.js
@@ -11,4 +11,8 @@ app.get("/about",function(req,res){
     res.render("about");
 });
 
+// app.listen(3000,function(){
+// 	console.log("Deploy app started");
+// });
+
 app.listen(process.env.PORT,process.env.IP);
