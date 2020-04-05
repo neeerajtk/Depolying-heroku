74d327a7a38c131252bc3d25e9d69942f5ac77ec
neeerajtk <neerajtk560@gmail.com>
2020-04-05 22:37:11 +0530

    basic directories added 3

--- app.js
+++ app.js
@@ -11,6 +11,6 @@ app.get("/about",function(req,res){
     res.render("about");
 });
 
-// app.listen(3000,function(){
-// 	console.log("Deploy basic app started");
-// });
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
